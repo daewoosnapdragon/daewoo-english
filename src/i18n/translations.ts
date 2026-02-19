86ca57f629e59c354ddd9067e3d935923cf405d4
daewoosnapdragon <daewoosnapdragon@gmail.com>
2026-02-19 23:24:23 +0900

    Update translations.ts

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -21,6 +21,7 @@ export const translations = {
       lessonPlans: 'Lesson Plans',
       teacherGuides: 'Teacher Guides',
       adminDashboard: 'Program Overview',
+      teacherGuides: 'Teacher Guides',
     },
     // Common
     common: {
@@ -180,6 +181,7 @@ export const translations = {
       lessonPlans: '수업 계획',
       teacherGuides: '교사 가이드',
       adminDashboard: '프로그램 개요',
+      teacherGuides: '교사 가이드',
     },
     common: {
       save: '저장',
